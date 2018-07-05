a27713c44ca2456b82e58a0f6a3868ed2852b020
Shanavas M <shanavas.m@techversantinc.com>
2018-07-05 14:48:39 +0530

    Read message from kafka and forward to web client
    Issues:
    1. only one client can connect at a time

--- apps/kafkachannel/consumers.py
+++ apps/kafkachannel/consumers.py
@@ -1,12 +1,24 @@
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from confluent_kafka import Consumer, KafkaError
 import json
+
+
+kafka_conf = {
+    'bootstrap.servers': 'localhost:9092',
+    'group.id': 'netbot',
+    'default.topic.config': {'auto.offset.reset': 'beginning'}
+}
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
+
+        self.kafkaConsumer = Consumer(kafka_conf)
+        self.kafkaConsumer.subscribe(['netbot-topic'])
+        self.kafkaStreaming = False
 
         # Join room group
         async_to_sync(self.channel_layer.group_add)(
@@ -17,6 +29,7 @@ class ChatConsumer(WebsocketConsumer):
         self.accept()
 
     def disconnect(self, close_code):
+        self.kafkaStreaming = False
         # Leave room group
         async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
@@ -25,23 +38,49 @@ class ChatConsumer(WebsocketConsumer):
 
     # Receive message from WebSocket
     def receive(self, text_data):
+        print('received message from socket')
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-
-        # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
             }
         )
 
     # Receive message from room group
     def chat_message(self, event):
-        message = event['message']
+        print('received message from groupd')
 
-        # Send message to WebSocket
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
+        # already streaming from kafka
+        if self.kafkaStreaming:
+            print('already streaming from kafka')
+            return
+
+        self.kafkaStreaming = True
+
+        while self.kafkaStreaming:
+            msg = self.kafkaConsumer.poll()
+
+            if msg.error():
+                if msg.error().code() == KafkaError._PARTITION_EOF:
+                    print('No more messages')
+                    continue
+                else:
+                    print(msg.error())
+                    break
+
+            print(msg.value().decode('utf-8'))
+            print('sending message to client')
+            # Send message to WebSocket
+            self.send(text_data=json.dumps({
+                'message': msg.value().decode('utf-8'),
+            }))
+
+        print('closing kafka stream')
+        self.kafkaConsumer.close()
+
+        # self.send(text_data=json.dumps({
+        #     'message': event['message'],
+        # }))
