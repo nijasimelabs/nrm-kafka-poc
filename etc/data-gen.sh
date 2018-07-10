97bae25d6af110278ebd8dcb22a524a8a8c4bda8
Shanavas M <shanavas.m@techversantinc.com>
2018-07-10 15:35:02 +0530

    Add max interval

--- etc/data-gen.sh
+++ etc/data-gen.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 CONFLUENT_BIN="/home/ubuntu/confluent/bin/"
-
+MAX_INTL="maxInterval=5000"
 
 start() {
 	echo "Clearing pervious logs...."
 	rm -f log/*.log
 
 	echo "Starting data generators...."
-	${CONFLUENT_BIN}ksql-datagen schema=wan-operational-db.avro format=delimited topic=wanoperationaldb key=seq  > /dev/null 2> log/wan.log &
+	${CONFLUENT_BIN}ksql-datagen schema=wan-operational-db.avro format=delimited topic=wanoperationaldb key=seq  ${MAX_INTL} > /dev/null 2> log/wan.log &
 	echo $! > log/wan.pid
-	${CONFLUENT_BIN}ksql-datagen schema=operational-scpc.avro format=delimited topic=operationalscpc key=link  > /dev/null 2> log/op.log &
+	${CONFLUENT_BIN}ksql-datagen schema=operational-scpc.avro format=delimited topic=operationalscpc key=link ${MAX_INTL} > /dev/null 2> log/op.log &
 	echo $! > log/op.pid
-	${CONFLUENT_BIN}ksql-datagen schema=traffic-classification.avro format=delimited topic=trafficclassification key=link  > /dev/null 2> log/tr-shift.log &
+	${CONFLUENT_BIN}ksql-datagen schema=traffic-classification.avro format=delimited topic=trafficclassification key=link ${MAX_INTL} > /dev/null 2> log/tr-shift.log &
 	echo $! > log/tr-class.pid
-	${CONFLUENT_BIN}ksql-datagen schema=traffic-shaping.avro format=delimited topic=trafficshaping key=link  > /dev/null 2> log/tr-class.log &
+	${CONFLUENT_BIN}ksql-datagen schema=traffic-shaping.avro format=delimited topic=trafficshaping key=link ${MAX_INTL} > /dev/null 2> log/tr-class.log &
 	echo $! > log/tr-shift.pid
 }
 
