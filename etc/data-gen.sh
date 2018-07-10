89d0bacbb78c9ea891252203c6433b9702378c01
Shanavas M <shanavas.m@techversantinc.com>
2018-07-10 14:58:53 +0530

    Update data-gen script

--- etc/data-gen.sh
+++ etc/data-gen.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
 CONFLUENT_BIN="/home/ubuntu/confluent/bin/"
 
-echo "Clearing pervious logs...."
-rm -f log/*.log
 
-${CONFLUENT_BIN}ksql-datagen schema=wan-operational-db.avro format=delimited topic=wanoperationaldb key=seq  > /dev/null 2> log/wan.log &
-${CONFLUENT_BIN}ksql-datagen schema=operational-scpc.avro format=delimited topic=operationalscpc key=link  > /dev/null 2> log/op.log &
-${CONFLUENT_BIN}ksql-datagen schema=traffic-classification.avro format=delimited topic=trafficclassification key=link  > /dev/null 2> log/tr-shift.log &
-${CONFLUENT_BIN}ksql-datagen schema=traffic-shaping.avro format=delimited topic=trafficshaping key=link  > /dev/null 2> log/tr-class.log &
+start() {
+	echo "Clearing pervious logs...."
+	rm -f log/*.log
+
+	echo "Starting data generators...."
+	${CONFLUENT_BIN}ksql-datagen schema=wan-operational-db.avro format=delimited topic=wanoperationaldb key=seq  > /dev/null 2> log/wan.log &
+	echo $! > log/wan.pid
+	${CONFLUENT_BIN}ksql-datagen schema=operational-scpc.avro format=delimited topic=operationalscpc key=link  > /dev/null 2> log/op.log &
+	echo $! > log/op.pid
+	${CONFLUENT_BIN}ksql-datagen schema=traffic-classification.avro format=delimited topic=trafficclassification key=link  > /dev/null 2> log/tr-shift.log &
+	echo $! > log/tr-class.pid
+	${CONFLUENT_BIN}ksql-datagen schema=traffic-shaping.avro format=delimited topic=trafficshaping key=link  > /dev/null 2> log/tr-class.log &
+	echo $! > log/tr-shift.pid
+}
+
+stop() {
+	echo "Killing data generators...."
+	kill -9 `cat log/wan.pid`
+	kill -9 `cat log/op.pid`
+	kill -9 `cat log/tr-class.pid`
+	kill -9 `cat log/tr-shift.pid`
+}
+
+
+case $1 in
+    "start" )
+        start ;;
+    "stop" )
+		stop ;;
+    * )
+		echo "Unknown command $1"
+esac
